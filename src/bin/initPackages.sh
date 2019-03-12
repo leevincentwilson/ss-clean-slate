e45a04f64cdd1553668f61de8fddb74ae4c0bd2a
leevincentwilson <lee.wilson@bath.edu>
2019-03-12 14:40:54 +0000

    Added in jest stuff to the npm dev add for linting reasons

--- src/bin/initPackages.sh
+++ src/bin/initPackages.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 yarn add react react-dom @material-ui/core @material-ui/icons typeface-roboto
 yarn add --dev ss-clean-babel ss-clean-lint ss-clean-webpack ss-clean-jest-enzyme
+yarn add --dev enzyme enzyme-adapter-react-16 enzyme-to-json
 node ./node_modules/ss-clean-slate/src/bin/init.js
